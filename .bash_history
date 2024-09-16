f3b766b1b3e639e058fb44a3968f69d7f78b3140
JacobLudwigson <jalu1895@colorado.edu>
2024-09-16 12:09:29 -0600

    Pushing from laptop to remote

---
+++ .bash_history
@@ -0,0 +1,110 @@
+pacman -S --needed base-devel mingw-w64-x86_64-toolchain
+[200~pacman -S mingw-w64-x86_64-freeglut
+~
+pacman -S mingw-w64-x86_64-freeglut
+ls
+cd ..
+ls
+cd jakal
+cd CSCI5229
+;s
+ls
+cd ..
+cd ..
+ls
+cd home
+cd !
+cd ~
+touch helloworld.c
+ls
+vim helloworld.c
+gcc helloworld.c
+./a.exe
+[200~pacman -S mingw-w64-x86_64-glew~
+pacman -S mingw-w64-x86_64-glew~
+pacman -S mingw-w64-x86_64-glew
+cd ..
+cd ..
+cd ..
+ls
+cd home
+cd jakal
+ls
+mkdir homework1
+cd homework1
+ls
+gcc gears.c
+gcc gears.c
+gcc -Wall -o gears gears.c  -lglfw -framework Cocoa -framework OpenGL -framework IOKit
+gcc -Wall -o gears gears.c  -lglfw -framework Cocoa -framework OpenGL
+gcc -Wall -o gears gears.c  -lglfw -lopengl32
+gcc -Wall -DUSEGLEW -o gear gears.c -lfreeglut -lglew32 -lglu32 -lopengl32 -lm
+./gear
+./gear
+gcc -Wall -DUSEGLEW -o gear gears.c -lfreeglut -lglew32 -lglu32 -lopengl32 -lm
+./gear
+./gear
+gcc -Wall -DUSEGLEW -o gear gears.c -lfreeglut -lglew32 -lglu32 -lopengl32 -lm
+./gear
+gcc -Wall -DUSEGLEW -o gear gears.c -lfreeglut -lglew32 -lglu32 -lopengl32 -lm
+./gear
+ls
+cd ex6
+ls
+make
+./a.exe
+ls
+./ex6.exe
+ls
+make
+./ex6.exe
+make
+./ex6.exe
+make
+./ex6.exe
+./ex6.exe
+make
+./ex6.exe
+cd ..
+ls
+cd homework2
+ls
+make
+make
+lorez.exe
+./lorez.exe
+cd ..
+ls
+cd ex6
+ls
+./ex6
+cd ..
+ls
+cd homework2
+ls
+make
+./lorez.exe
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+./lorez.exe
+make
+make
+./lorez.exe
---
+++ homework2/lorez.c
@@ -0,0 +1,263 @@
+/*
+ *  Coordinates
+ *
+ *  Display 2, 3 and 4 dimensional coordinates in 3D.
+ *
+ *  Key bindings:
+ *  1      2D coordinates
+ *  2      2D coordinates with fixed Z value
+ *  3      3D coordinates
+ *  4      4D coordinates
+ *  +/-    Increase/decrease z or w
+ *  arrows Change view angle
+ *  0      Reset view angle
+ *  ESC    Exit
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#ifdef USEGLEW
+#include <GL/glew.h>
+#endif
+//  OpenGL with prototypes for glext
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+// Tell Xcode IDE to not gripe about OpenGL deprecation
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#else
+#include <GL/glut.h>
+#include <math.h>
+#endif
+//  Default resolution
+//  For Retina displays compile with -DRES=2
+#ifndef RES
+#define RES 1
+#endif
+
+//  Globals
+int th=0;       // Azimuth of view angle
+int ph=0;       // Elevation of view angle
+double z=0;     // Z variable
+double w=1;     // W variable
+double dim=2;   // Dimension of orthogonal box
+int mode = 1;
+const char* text[] = {"","2D","3D constant Z","3D","4D"};  // Dimension display text
+
+/*  Lorenz Parameters  */
+double s  = 10;
+double b  = 2.6666;
+double r  = 28;
+/*
+ *  Convenience routine to output raster text
+ *  Use VARARGS to make this more flexible
+ */
+#define LEN 8192  // Maximum length of text string
+void Print(const char* format , ...)
+{
+   char    buf[LEN];
+   char*   ch=buf;
+   va_list args;
+   //  Turn the parameters into a character string
+   va_start(args,format);
+   vsnprintf(buf,LEN,format,args);
+   va_end(args);
+   //  Display the characters one at a time at the current raster position
+   while (*ch)
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
+
+/*
+ *  Print message to stderr and exit
+ */
+void Fatal(const char* format , ...)
+{
+   va_list args;
+   va_start(args,format);
+   vfprintf(stderr,format,args);
+   va_end(args);
+   exit(1);
+}
+
+/*
+ *  Check for OpenGL errors
+ */
+void ErrCheck(const char* where)
+{
+   int err = glGetError();
+   if (err) fprintf(stderr,"ERROR: %s [%s]\n",gluErrorString(err),where);
+}
+
+/*
+ *  Display the scene
+ */
+void display()
+{
+   //  Clear the image
+   glClear(GL_COLOR_BUFFER_BIT);
+   //  Reset previous transforms
+   glLoadIdentity();
+   //  Set view angle
+   glRotated(ph,1,0,0);
+   glRotated(th,0,1,0);
+  
+   
+   //  Draw 10 pixel yellow points
+   glColor3f(1,1,0);
+   glPointSize(1);
+   glBegin(GL_LINES);
+   int i;
+   /*  Coordinates  */
+   double x = 1;
+   double y = 1;
+   double zl = 1;
+   double vectorLength = 1;
+   /*  Time step  */
+   double dt = 0.001;
+   //  Two dimensions
+   for (i=0;i<50000;i++)
+   {
+      // printf("%d ", i);
+      glColor3f(i,i,i);
+      double dx = s*(y-x);
+      double dy = x*(r-zl)-y;
+      double dz = x*y - b*zl;
+      x += dt*dx;
+      y += dt*dy;
+      zl += dt*dz;
+      vectorLength = sqrt((x*x) + (y*y) + (zl*zl));
+      vectorLength = 30;
+      glVertex3d(x/vectorLength,y/vectorLength,zl/vectorLength);
+   }
+   glEnd();
+   //  Draw axes in white
+   glColor3f(1,1,1);
+   glBegin(GL_LINES);
+   glVertex3d(0,0,0);
+   glVertex3d(1,0,0);
+   glVertex3d(0,0,0);
+   glVertex3d(0,1,0);
+   glVertex3d(0,0,0);
+   glVertex3d(0,0,1);
+   glEnd();
+   //  Label axes
+   glRasterPos3d(1,0,0);
+   Print("X");
+   glRasterPos3d(0,1,0);
+   Print("Y");
+   glRasterPos3d(0,0,1);
+   Print("Z");
+   //  Display parameters
+   glWindowPos2i(5,5);
+   Print("View Angle=%d,%d",th,ph);
+   // if (mode==2)
+   //    Print("  z=%.1f",z);
+   // else if (mode==4)
+   //    Print("  w=%.1f",w);
+   //  Flush and swap
+   ErrCheck("display");
+   glFlush();
+   glutSwapBuffers();
+}
+
+/*
+ *  GLUT calls this routine when a key is pressed
+ */
+void key(unsigned char ch,int x,int y)
+{
+   if (ch == ',')
+   {
+      // if (mode==2)
+      //    z += 0.1;
+      // else
+      //    w += 0.1;
+   }
+   else if (ch == '.')
+   {
+      // if (mode==2)
+      //    z -= 0.1;
+      // else
+      //    w -= 0.1;
+   }
+   else if (ch == '/'){
+      
+   }
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void special(int key,int x,int y)
+{
+   //  Right arrow key - increase azimuth by 5 degrees
+   if (key == GLUT_KEY_RIGHT)
+      th += 5;
+   //  Left arrow key - decrease azimuth by 5 degrees
+   else if (key == GLUT_KEY_LEFT)
+      th -= 5;
+   //  Up arrow key - increase elevation by 5 degrees
+   else if (key == GLUT_KEY_UP)
+      ph += 5;
+   //  Down arrow key - decrease elevation by 5 degrees
+   else if (key == GLUT_KEY_DOWN)
+      ph -= 5;
+   //  Keep angles to +/-360 degrees
+   th %= 360;
+   ph %= 360;
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+   //  Set the viewport to the entire window
+   glViewport(0,0, RES*width,RES*height);
+   //  Tell OpenGL we want to manipulate the projection matrix
+   glMatrixMode(GL_PROJECTION);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Orthogonal projection box adjusted for the
+   //  aspect ratio of the window
+   double asp = (height>0) ? (double)width/height : 1;
+   glOrtho(-asp*dim,+asp*dim, -dim,+dim, -dim,+dim);
+   //  Switch to manipulating the model matrix
+   glMatrixMode(GL_MODELVIEW);
+   //  Undo previous transformations
+   glLoadIdentity();
+}
+
+/*
+ *  Start up GLUT and tell it what to do
+ */
+int main(int argc,char* argv[])
+{
+  //  Initialize GLUT and process user parameters
+   glutInit(&argc,argv);
+   //  Request double buffered, true color window 
+   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+   //  Request 500 x 500 pixel window
+   glutInitWindowSize(1920,1080);
+   //  Create the window
+   glutCreateWindow("Jacob Ludwigson");
+#ifdef USEGLEW
+   //  Initialize GLEW
+   if (glewInit()!=GLEW_OK) Fatal("Error initializing GLEW\n");
+#endif
+   //  Tell GLUT to call "display" when the scene should be drawn
+   glutDisplayFunc(display);
+   //  Tell GLUT to call "reshape" when the window is resized
+   glutReshapeFunc(reshape);
+   //  Tell GLUT to call "special" when an arrow key is pressed
+   glutSpecialFunc(special);
+   //  Tell GLUT to call "key" when a key is pressed
+   glutKeyboardFunc(key);
+   //  Pass control to GLUT so it can interact with the user
+   glutMainLoop();
+   //  Return code
+   return 0;
+}
